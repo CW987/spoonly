160b856cda2b0afe61420eee274b1b2ea8303c60
“hxc338” <hxc338@student.bham.ac.uk>
2025-02-27 17:20:19 +0000

    added basic clerk functionality

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,28 @@
+import {
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { auth } from '@clerk/nextjs/server';
+import Link from 'next/link';
+
+export default function Header(){
+    return(
+        <>
+            <div className='flex justify-between m-4 p-4'>
+                <Link href={"/"}>Home</Link>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+                <SignedOut>
+                    <SignUpButton/>
+                    <SignInButton/>
+                </SignedOut>
+            </div>
+        
+        </>
+    )
+}
