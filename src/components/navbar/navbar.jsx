0bc0e52aa4d5ff16a59dcff7f7d34a959a7f8d76
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2025-02-28 13:09:45 +0000

    Created Home Page Created top creations and recipes, my creations and leaderboards fill in Moved sign in/up and profile button to nav bar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import FillInLogo from "./FillInLogo.png";
 import Links from "./links";
 import SearchBar from "./searchbar";
+import SignInUp from "./Header";
 
 export default function NavBar() {
   return (
@@ -16,6 +17,7 @@ export default function NavBar() {
       <Links />
       <SearchBar />
       <DropDown />
+      <SignInUp />
       <Separator my=".5" size="4" id="NavSep" />
     </div>
   );
