3baef063a6fa2b2efe1cd7de66c4d610027db4ea
Natasha C <n.coward@hotmail.com>
2025-02-28 10:25:02 +0000

    Initial commit - started README and applied Radix theme

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
-import { Button } from "@radix-ui/themes";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 export default function HomePage() {
   return (
     <div>
+      <Flex direction="column" gap="2">
+        <Text>Welcome to Spoonly</Text>
+      </Flex>
       <Button>Hello</Button>
     </div>
   );
