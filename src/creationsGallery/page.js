3e56369bc123d9c5a56dac50e9980847c221aa93
Curtis Witter <inward-panting0f@icloud.com>
2025-02-28 10:05:03 +0000

    Basic pages and routes

---
+++ src/creationsGallery/page.js
@@ -0,0 +1,7 @@
+export default function creationsGallery(){
+    return(
+        <>
+        <h1>Creations Gallery</h1>
+        </>
+    )
+}
---
+++ src/creationsRecipe/page.js
@@ -0,0 +1,7 @@
+export default function creationsRecipe(){
+    return(
+        <>
+        <h1>Creations Recipe</h1>
+        </>
+    )
+}
