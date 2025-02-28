c61f81742d631251775226881baec4783b5d1f1a
Curtis Witter <inward-panting0f@icloud.com>
2025-02-28 14:45:11 +0000

    NewestCreations component added

---
+++ src/components/home/newestCreations.jsx
@@ -0,0 +1,15 @@
+import { Card, Box, Separator, Heading, Text } from "@radix-ui/themes";
+
+export default function NewestCreations() {
+  return (
+    <div id="NewestRecipes">
+      <Box>
+        <Card>
+          <Heading>Newest Creations</Heading>
+          <Separator my="3" size="4"></Separator>
+          <Text>Fill in for new Creations</Text>
+        </Card>
+      </Box>
+    </div>
+  );
+}
