ed40a4a4ae11182b81cf75308e52d7f472810029
Curtis Witter <inward-panting0f@icloud.com>
2025-03-03 16:12:11 +0000

    Capitalisation fixes - commented out db connection as it was breaking the app for testing

--- src/app/creationsMain/page.js
+++ src/app/creationsMain/page.js
@@ -1,12 +1,12 @@
 import TopCreations from "@/components/home/Creations/topCreations";
 import NewestCreations from "@/components/home/Creations/newestCreations";
-import creationsFilters from "@/components/home/Creations/creationsFilters";
+import CreationsFilters from "@/components/home/Creations/creationsFilters";
 
 export default function creationsMain() {
   return (
     <>
       <div>
-        <creationsFilters />
+        <CreationsFilters />
       </div>
       <div>
         <TopCreations />
