0626cb9a3cf068352864eb8b7cd58bc5649cb33f
Curtis Witter <inward-panting0f@icloud.com>
2025-03-04 09:35:32 +0000

    Recipes Main and components

--- src/components/home/Recipes/newestRecipes.jsx
+++ src/components/home/Recipes/newestRecipes.jsx
@@ -1,13 +1,13 @@
 import { Card, Box, Separator, Heading, Text } from "@radix-ui/themes";
 
-export default function RecipeFilters() {
+export default function NewestRecipes() {
   return (
-    <div id="Filters">
+    <div id="NewestRecipes">
       <Box>
         <Card>
-          <Heading>Recipe Filters</Heading>
+          <Heading>Newest Recipes</Heading>
           <Separator my="3" size="4"></Separator>
-          {/* Filter pane goes here */}
+          <Text>Fill in for new Recipes</Text>
         </Card>
       </Box>
     </div>
