79d06954ebd01e9530ac8454f9fd734e0ef7b8d8
Curtis Witter <inward-panting0f@icloud.com>
2025-03-04 11:51:09 +0000

    Dynamic routing for recipes

---
+++ src/app/creationsRecipe/[id]/page.js
@@ -0,0 +1,37 @@
+// import SimilarCreations from "@/components/home/Creations/similarCreations";
+// import CreationsView from "@/components/home/Creations/creationsView";
+// import Comments from "@/components/home/comments";
+
+// export default function creationsRecipe({ params }){
+//     const
+//     return(
+//         <>
+//         <SimilarCreations />
+//         <CreationsView />
+//         <Comments />
+//         </>
+//     )
+// }
+
+
+
+
+
+
+// import UserCard from "@/components/userpage/UserCard";
+// import "./userpage.css";
+// import UserRecipes from "@/components/userpage/UserRecipes";
+// import UserCreations from "@/components/userpage/userCreations";
+
+// export default async function UserPage({ params }) {
+//   const brokenParams = await params.id;
+//   return (
+//     <div id="UserPageMain">
+//       <UserCard params={brokenParams} />
+//       <div id="UserContent">
+//         <UserRecipes params={brokenParams} />
+//         <UserCreations params={brokenParams} />
+//       </div>
+//     </div>
+//   );
+// }
