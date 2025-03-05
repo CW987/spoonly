2405df0112045916d78bc1b1beefd2002bfd8eff
“hxc338” <hxc338@student.bham.ac.uk>
2025-03-05 14:19:36 +0000

    added comment form

--- src/components/home/Recipes/allRecipes.jsx
+++ src/components/home/Recipes/allRecipes.jsx
@@ -1,5 +1,7 @@
-import { Card, Box, Separator, Heading, Text } from "@radix-ui/themes";
+import { Card, Box, Separator, Heading, Text, Inset, Strong } from "@radix-ui/themes";
+import "../../../app/recipes/recipe.css"
 import { db } from "@/utils/dbConnection";
+import Link from "next/link";
 
 export default async function AllRecipes() {
 
@@ -10,28 +12,40 @@ export default async function AllRecipes() {
   console.log(wrangledRecipes)
 
   return (
-    <div id="AllRecipes">
-      <Box>
-        <Card>
-          <Heading>Recipes Gallery</Heading>
-          <Separator my="3" size="4"></Separator>
-          <Text>{wrangledRecipes.map((item)=>(
-            <div key={item.recipe_id}>
-              <h2 className="text-xl">{item.recipe_name}</h2>
-              <p>Rating ⭐️:  {item.rating}/5</p>
-              <p>Cuisine 🌍: {item.cuisine}</p>
-              <p>Dish Type 🍽️: {item.dish_type}</p>
-              <p>⏳{item.cook_time} mins</p>
-              <p>Servings 🧍‍♀️: {item.servings}</p>
+    <div id="allRecipes">
+      {wrangledRecipes.map((recipe)=>(
+        <div className="recipeBoxContainer" key={recipe.id}>
+          <Link href={`/recipes/${recipe.recipe_id}`}>
+          <Box className="recipeBox" maxWidth="400px">
+            <Card className="recipe" size="2">
+              <Inset clip="padding-box" side="top" pb="current">
+                <img
+                  src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                  alt="Bold typography"
+                  style={{
+                    display: "block",
+                    objectFit: "cover",
+                    width: "100%",
+                    height: 180,
+                    backgroundColor: "var(--gray-5)",
+                  }}
+                />
+              </Inset>
+              <Text>
+                <Strong>{recipe.recipe_name}</Strong>
+                <p>{recipe.cook_time} mins</p>
+                <p>Rating: {recipe.rating} / 5 </p>
+              </Text>
+            </Card>
+          </Box>
+          </Link>
 
-            </div>
 
-          ))}
+          </div>
+      ))}
 
 
-          </Text>
-        </Card>
-      </Box>
+
     </div>
   );
 }
