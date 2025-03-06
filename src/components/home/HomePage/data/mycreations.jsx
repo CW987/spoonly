6f67a623f264749b56e400ccca223ad0f5d75e78
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2025-03-06 10:19:43 +0000

    Added signed in function to comments on recipes page Added signed in function to my creations on home page Adjusted portrait media queries

--- src/components/home/HomePage/data/mycreations.jsx
+++ src/components/home/HomePage/data/mycreations.jsx
@@ -25,29 +25,29 @@ export default async function MyCreationsData({ params }) {
     <div>
       {brokenCreeations.map((recipe) => (
         <div className="recipeBoxContainer" key={recipe.creations_id}>
-          {/* <Link href={`/recipes/${recipe.creation_id}`}> */}
-          <Box className="recipeBox" maxWidth="500px">
-            <Card className="recipe" size="2">
-              <Inset clip="padding-box" side="top" pb="current">
-                <img
-                  src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
-                  alt="Bold typography"
-                  style={{
-                    display: "block",
-                    objectFit: "cover",
-                    width: "100%",
-                    height: 180,
-                    backgroundColor: "var(--gray-5)",
-                  }}
-                />
-              </Inset>
-              <Text>
-                <Strong>{recipe.creation_name}</Strong>
-                <p>Rating: {recipe.rating} / 5 </p>
-              </Text>
-            </Card>
-          </Box>
-          {/* </Link> */}
+          <Link href={`/recipes/${recipe.creation_id}`}>
+            <Box className="recipeBox" maxWidth="500px">
+              <Card className="recipe" size="2">
+                <Inset clip="padding-box" side="top" pb="current">
+                  <img
+                    src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                    alt="Bold typography"
+                    style={{
+                      display: "block",
+                      objectFit: "cover",
+                      width: "100%",
+                      height: 180,
+                      backgroundColor: "var(--gray-5)",
+                    }}
+                  />
+                </Inset>
+                <Text>
+                  <Strong>{recipe.creation_name}</Strong>
+                  <p>Rating: {recipe.rating} / 5 </p>
+                </Text>
+              </Card>
+            </Box>
+          </Link>
         </div>
       ))}
     </div>
