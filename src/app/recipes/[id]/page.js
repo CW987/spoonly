afcdd187c618d707ff5a24ddb473f4a17056048f
Thannis86 <cameron.burchnall@ntlworld.com>
2025-03-06 10:20:56 +0000

    Added signed in function to comments on recipes page (#43)
    Added signed in function to my creations on home page
    Adjusted portrait media queries

--- src/app/recipes/[id]/page.js
+++ src/app/recipes/[id]/page.js
@@ -1,10 +1,11 @@
 import RecipeCommentsForm from "@/components/recipes/CommentsForm/RecipeCommentsForm";
 import RecipeInfo from "@/components/recipes/individualPages/Recipe";
 import { db } from "@/utils/dbConnection";
-import "../recipe.css"
+import "../recipe.css";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { SignedIn } from "@clerk/nextjs";
 
 export default async function RecipePage({ params }) {
   const recipeParams = await params;
@@ -14,50 +15,53 @@ export default async function RecipePage({ params }) {
   console.log(paramsId);
 
   const recipeComment = await db.query(
-    `SELECT recipe_comments_id, recipe_id, comment_content FROM recipe_comments WHERE recipe_id = $1`, [recipeParams.id])
-  console.log(recipeComment)
-  const wrangledRecipeComment = recipeComment.rows
-  console.log(wrangledRecipeComment)
+    `SELECT recipe_comments_id, recipe_id, comment_content FROM recipe_comments WHERE recipe_id = $1`,
+    [recipeParams.id]
+  );
+  console.log(recipeComment);
+  const wrangledRecipeComment = recipeComment.rows;
+  console.log(wrangledRecipeComment);
 
-  async function handleDeleteRecipe(){
-    "use server"
-    await db.query(`DELETE FROM recipes WHERE recipe_id = $1`, [recipeParams.id])
-    revalidatePath(`/recipes`)
-    redirect(`/recipes`)
-}
-
+  async function handleDeleteRecipe() {
+    "use server";
+    await db.query(`DELETE FROM recipes WHERE recipe_id = $1`, [
+      recipeParams.id,
+    ]);
+    revalidatePath(`/recipes`);
+    redirect(`/recipes`);
+  }
 
   return (
     <>
       <div className="myRecipePage">
         <RecipeInfo params={paramsId} />
-        <RecipeCommentsForm paramId={paramsId} />
+        <SignedIn>
+          <RecipeCommentsForm paramId={paramsId} />
+        </SignedIn>
 
         <div className="recipeCommentContainer">
-            {wrangledRecipeComment.map((comment)=>(
-              <div key={comment.recipe_comments_id} className="recipeComment">
-                <p>💬: {comment.comment_content}</p>
-              </div>
-            ))}
+          {wrangledRecipeComment.map((comment) => (
+            <div key={comment.recipe_comments_id} className="recipeComment">
+              <p>💬: {comment.comment_content}</p>
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="buttons">
-                <div className="deleteRecipe">
-                    <form action={handleDeleteRecipe}>
-                        <button
-                            type="submit">
-                                Delete this recipe</button>
-                    </form>
-                </div>
+        <div className="deleteRecipe">
+          <form action={handleDeleteRecipe}>
+            <button type="submit">Delete this recipe</button>
+          </form>
+        </div>
 
-                <div className="updateLink">
-                {/* <Link
+        <div className="updateLink">
+          {/* <Link
                     href={`/creations/${creationParams.id}/update-creation`}>
                     Update creation
                 </Link> */}
-                </div>
-            </div>
+        </div>
+      </div>
     </>
   );
 }
