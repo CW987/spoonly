56b54fdad15a2c32d6a4cef4ec5eaf4bdc6fa92f
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2025-03-06 15:38:01 +0000

    fixes

--- src/components/home/HomePage/data/toprecipes.jsx
+++ src/components/home/HomePage/data/toprecipes.jsx
@@ -24,7 +24,7 @@ export default async function TopRecipesData() {
               <Card className="recipe" size="2">
                 <Inset clip="padding-box" side="top" pb="current">
                   <img
-                    src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                    src={recipe.image}
                     alt="Bold typography"
                     style={{
                       display: "block",
